a60c81cb8dcf1fe0ec7ffdf456ee2d88092fbe0c
E-Thorsen <Ethorsn@gmail.com>
2015-11-22 10:49:49 +0100

    Change for AnscombeRes
    small change for anscombe

--- hhh4DevianceRes.R
+++ hhh4DevianceRes.R
@@ -2,11 +2,13 @@ library("stats")
 library("dplyr")
 library("reshape2")
 library("ggplot2")
+library("surveillance")
 #Load models
 load("Models/hhh4Model.RData")
 
 # extracting residuals
 Deviance.resid.ASIS <- residuals(hhh4Model4.wo.strat.od,"deviance")
+#H%*%melt(hhh4Model4.wo.strat.od$stsObj@observed)$value - fitted.values(Deviance.resid.ASIS)
 colNam <- colnames(Deviance.resid.ASIS)
 # Transforming AsIs object into list
 Deviance.resid.ASIS <- lapply(1:16, function(i) Deviance.resid.ASIS[, i])
@@ -22,13 +24,23 @@ trans <- as.character(resid.long$variable) %>%
 trans <- do.call("rbind",trans)
 resid.long$Sex <- trans[,1]
 resid.long$Age <- trans[,2]
-#resid.long$Type <- as.factor("Deviance") Only plotting deviance.
-# index.
+resid.long$Type <- as.factor("Deviance")
+# index used for plotting.
 resid.long$index <- 1:(dim(resid.long)[1]/2)
 
-ggplot(resid.long) + 
-  geom_point(aes(x=index, y=value, color=Age)) + 
-  facet_grid(~Sex) +
-  ylab("Deviance residual") +
-  scale_color_brewer(palette=15)
-  
+###############################
+# Anscombe residuals (EXPERIMENTAL!)
+# Data.frame to get model.matrix
+
+#pdf("Figures/AnscombeResidExperimental.pdf", width = 6, height = 4, paper = 'special')
+#ggplot(data = plot.data, aes(y = res, x = 1:length(res), col = Age:Sex)) +
+#  geom_point() +
+#  xlab("Observation") + 
+#  ylab("Anscombe residuals") +
+#  theme_bw()
+#dev.off()
+
+
+
+
+min(H%*%y - mu)
